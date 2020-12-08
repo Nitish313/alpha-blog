b3be0ee5590451962481bf0152ec87c41b6fa4ab
Nitish313 <student.nitishanand@gmail.com>
2020-12-08 12:26:08 +0530

    Add password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 
+  has_secure_password
+  
   private 
     def downcase_email
       self.email = email.downcase
