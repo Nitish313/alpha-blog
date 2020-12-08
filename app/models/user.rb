c2cacc8275ab1290210959bcb613eeb9089c4a8a
Nitish313 <student.nitishanand@gmail.com>
2020-12-08 12:07:42 +0530

    display username in articles index, show; downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
 
   VALID_EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+  private 
+    def downcase_email
+      self.email = email.downcase
+    end
 end
