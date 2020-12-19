77c5d887f2f351402dc2d47cb4c248e784c0441a
Nitish313 <student.nitishanand@gmail.com>
2020-12-19 15:04:52 +0530

    Refactorand add edit and update actions for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :admin_only, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -19,9 +20,21 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category name successfully updated"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def category_params
       params.require(:category).permit(:name)
@@ -32,6 +45,10 @@ class CategoriesController < ApplicationController
         flash[:warning] = "Only admins can perform that action"
         redirect_to categories_path
       end
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
     end
   
 end
